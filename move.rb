7c3566b44b8896073264b6a79d781472bc87fecf
alexpchin <alex@womapp.com>
2014-08-14 13:59:29 +0100

    Just before adding remote repo

--- move.rb
+++ move.rb
@@ -2,7 +2,7 @@ class Move
   attr_accessor :orientation, :coordinates
 
   def initialize(options={})
-    @orientation = options[:orientation]      # Compass e.g. N,S,E,W (String)
+    @orientation = options[:orientation]      # Compass e.g. n,s,e,w (String)
     @coordinates = [options[:x], options[:y]] # Coordinate (Array)
   end
 
