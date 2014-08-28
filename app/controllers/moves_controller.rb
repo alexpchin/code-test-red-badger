3ca2ed6d618caa85d947ad4bfff81d205df7a854
alexpchin <alex@womapp.com>
2014-08-28 15:41:36 +0100

    Improving moves controller

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -8,11 +8,13 @@ class MovesController < ApplicationController
   end
 
   def create
-    if params.has_key?(:robot_instruction)
-      @move = @robot.moves.new(@robot.select_move(params[:robot_instruction]))
-    else
-      @move = @robot.moves.new(move_params)
+    if params.has_key?(:robot_instruction) 
+      move = @robot.select_move(params[:robot_instruction]
+    else 
+      move = move_params
     end
+    @move = @robot.moves.new(move)
+    
     if @move.save
       flash[:notice] = 'Move was successfully created.'
       respond_with @world 
